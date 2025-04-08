f2d011aece3ab483292b332976509563f9e1e5cf
sehyeon7 <worldjade07@gmail.com>
2025-04-08 15:41:05 +0900

    access token 만료 기간 늘리기

--- kickit/settings.py
+++ kickit/settings.py
@@ -272,7 +272,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 from datetime import timedelta 
 REST_USE_JWT = True 
-SIMPLE_JWT = {  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30), 
+SIMPLE_JWT = {  'ACCESS_TOKEN_LIFETIME': timedelta(hours=2), 
                 'REFRESH_TOKEN_LIFETIME': timedelta(days=7), 
                 'ROTATE_REFRESH_TOKENS': True, 
                 'BLACKLIST_AFTER_ROTATION': True, 
