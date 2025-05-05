b8aa141c509c6f40cae487ff02cc1585e6cfbdae
sehyeon7 <worldjade07@gmail.com>
2025-05-06 02:08:34 +0900

    firebase 자격증명 : 파일 permission 풀어줌

--- .platform/hooks/prebuild/01_copy_firebase.sh
+++ .platform/hooks/prebuild/01_copy_firebase.sh
@@ -3,14 +3,15 @@ set -euxo pipefail
 
 # 1) /etc/secrets 준비
 mkdir -p /etc/secrets
-chmod 700 /etc/secrets
+chmod 755 /etc/secrets
 
 # 2) AWS Secrets Manager에서 JSON을 가져와 저장
 aws --region ap-northeast-2 secretsmanager get-secret-value \
   --secret-id firebase/credentials \
-  --query SecretString --output text \
+  --query SecretString \
+  --output text \
   > /etc/secrets/firebase.json
 
 # 3) 권한 타이트닝
-chmod 600 /etc/secrets/firebase.json
+chmod 644 /etc/secrets/firebase.json
 chown root:root /etc/secrets/firebase.json
